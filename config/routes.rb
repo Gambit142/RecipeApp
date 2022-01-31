21eb27b1f42669d741570c3be168386540725aed
brianorora <brianorora@gmail.com>
2022-01-31 19:07:30 +0300

    Add routes for recipes & foods
    - Create controller for recipes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :recipes, except: [ :update ]
+  resources :foods, except: [ :show, :update]
+
+  get 'public_recipes', to: 'recipes#public_recipes', as: :public_recipes
+
   root 'home#index'
 
 end
