d87122a07dc435ddfbec786328132751c3082fe4
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-31 17:31:15 +0100

    Create Foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
