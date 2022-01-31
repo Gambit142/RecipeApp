dc1bd74f52b26dc7baa58a195d6d3b7fb79a909c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-31 17:42:43 +0100

    Add general_shopping_list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  get 'foods/index'
-  get 'foods/new'
-  get 'foods/create'
-  get 'foods/destroy'
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   resources :recipes, except: [ :update ]
   resources :foods, except: [ :show, :update]
-
+  
+  get 'general_shopping_list', to: 'general_shopping_list#index', as: :shopping_list
   get 'public_recipes', to: 'recipes#public_recipes', as: :public_recipes
 
-  root 'home#index'
-
+  root 'general_shopping_list#index'
 end
