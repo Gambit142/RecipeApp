3431c4c7cb167ab0b1dabb0b738bb25c6f28530d
brianorora <brianorora@gmail.com>
2022-02-01 23:15:06 +0300

    Add authorization ability for foods_recipe

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Food
+    can :read, Recipe, public: true
     can :manage, Food, user: user
     can :manage, Recipe, user: user
     can :manage, Inventory, user: user
-    can :read, Food
-    can :read, Recipe, public: true
+    return unless user
+
+    can :manage, FoodsRecipe if user
   end
 end
