d625920e2f67aad8059c0d0ba9d683bd724d376c
brianorora <brianorora@gmail.com>
2022-02-01 17:41:25 +0300

    Implement `#destroy` action for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,20 @@
 class RecipesController < ApplicationController
+  load_and_authorize_resource
   def create; end
 
   def new; end
 
-  def index; end
+  def index
+    @recipes = Recipe.where(user: current_user)
+  end
 
   def show; end
 
-  def destroy; end
+  def destroy
+    @recipe.destroy!
+    flash[:notice] = 'Deleted Recipe!'
+    redirect_to recipes_url
+  end
 
   def public_recipes; end
 end
