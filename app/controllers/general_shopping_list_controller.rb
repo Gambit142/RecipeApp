1a74931c3e3fa707905d237b87bca2a3f97c6f1a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-02 21:58:52 +0100

    Query the database to get data for the shopping list view

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,10 +1,30 @@
 class GeneralShoppingListController < ApplicationController
   # load_and_authorize_resource
+  before_action :retrieve_missing_items
   def index
-    foods = Food.joins :ingredients, :foods_inventories
-    list_items = foods.pluck('foods_inventories.quantity, foods_recipes.quantity, foods.name, foods.price')
+    retrieve_missing_items
+  end
+
+  # def sum_total_amount
+  #   @total_amount = 0
+  #   missing_items.each |item| do
+  #     @total_amount += ((item[1] - item[0]) * item[3]))
+  #   end
+  #   @total_amount
+  # end
+
+  def retrieve_missing_items
+    id = current_user.id
+    sql = "select foods_inventories.quantity, foods_recipes.quantity, foods.name, foods.price, foods.measurement_unit from recipes join foods_recipes on foods_recipes.recipe_id = recipes.id left join foods on foods.id = foods_recipes.food_id join foods_inventories on foods_inventories.food_id = foods.id where recipes.user_id = #{id}"
+    records_array = ActiveRecord::Base.connection.execute(sql)
+
+    p records_array.values.class
+    list_items = records_array.values
+
     @missing_items = list_items.select do |list_item|
       list_item[0] < list_item[1]
     end
+
+    @missing_items
   end
 end
