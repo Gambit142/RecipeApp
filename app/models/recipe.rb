50a88e664120e02e983f93e5a42dafe9022e60c0
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-02 14:08:31 +0100

    Fix all linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -12,11 +12,11 @@ class Recipe < ApplicationRecord
     foods_recipes.create!(food: food, quantity: quantity) unless already_exists
   end
 
-  def get_total_food_items
+  def total_food_items
     foods.size
   end
 
-  def get_total_price
+  def total_price
     total = 0
     foods_recipes.each do |food_recipe|
       total += food_recipe.food.price * food_recipe.quantity
