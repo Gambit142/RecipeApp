83fe293726ddacefce72da14d7a555fb34e397ca
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-02 11:32:46 +0100

    Write methods that gets the total food items and total prices for those items

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,4 +11,16 @@ class Recipe < ApplicationRecord
     already_exists = foods.find_by(name: food.name)
     foods_recipes.create!(food: food, quantity: quantity) unless already_exists
   end
+
+  def get_total_food_items
+    foods.size
+  end
+
+  def get_total_price
+    total = 0
+    foods_recipes.each do |food_recipe|
+      total += food_recipe.food.price * food_recipe.quantity
+    end
+    total
+  end
 end
